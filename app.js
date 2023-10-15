be572ebca0d272516b664ade1028ece4cc88c1a4
Namit200323 <95710982+Namit200323@users.noreply.github.com>
2023-10-15 23:38:10 +0530

    Completed basic crud

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+// This is the change
 const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const Restr = require('./models/restr');
+const methodOverride = require('method-override');
 
 main().catch(err => console.log(err));
 
@@ -16,6 +18,9 @@ async function main() {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded());
+app.use(methodOverride('_method'));
+
 app.get('/',(req,res)=>{
     res.render('home');
 })
@@ -23,6 +28,38 @@ app.get('/',(req,res)=>{
 app.get('/restr',async(req,res)=>{
     const rests = await Restr.find({});
     res.render('restr/index',{rests})
+})
+
+app.get('/restr/new',(req,res)=>{
+    res.render('restr/new');
+})
+
+app.post('/restr',async(req,res)=>{
+    const restr = new Restr(req.body.restr);
+    await restr.save();
+    res.redirect(`/restr/${restr._id}`);
+})
+
+app.get('/restr/:id',async(req,res)=>{
+    const rest = await Restr.findById(req.params.id);
+    res.render('restr/show',{rest});
+})
+
+app.get('/restr/:id/edit',async(req,res)=>{
+    const rest = await Restr.findById(req.params.id);
+    res.render('restr/edit',{rest});
+})
+
+app.put('/restr/:id',async(req,res)=>{
+    const {id} = req.params;
+    const restr = await Restr.findByIdAndUpdate(id,{...req.body.restr});
+    res.redirect(`/restr/${restr._id}`);
+})
+
+app.delete('/restr/:id',async(req,res)=>{
+    const {id} = req.params;
+    await Restr.findByIdAndDelete(id);
+    res.redirect('/restr');
 })
 
 app.listen(200,()=>{
